1c3ca84f290f8b789299e7c27d91a40ae7e37b86
Pvpexmc <55208721+Pvpexmc@users.noreply.github.com>
2019-09-11 22:44:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
-const devs = ['389090790984515594'];
+const devs = ['519141956677271572'];
 const db = require('quick.db');
-const premium = ['470896018603376640']
+const premium = ['519141956677271572']
 const client = new Discord.Client();   
 const bot = new Discord.Client();   
 const giphy = require('giphy-api')();    
@@ -27,7 +27,7 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
 const dateFormat = require('dateformat'); 
 const pretty = require('pretty-ms') 
-const prefix = '$';
+const prefix = '*';
 var table = require('table').table
 var ti={}  
 ,spee={}
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['$help','Type $help'];
+    var setGame = ['*help','PvpEx Best Server Minecraft '];
     var i = -1;
     var j = 0;
     setInterval(function (){
@@ -404,7 +404,7 @@ client.on('message', message => {
                 timestamp: new Date(),
                 footer: {
                   icon_url: client.user.avatarURL,
-                  text: "3bker Community ©"
+                  text: "PvPEx Community ©"
                 }
             }}
             )
